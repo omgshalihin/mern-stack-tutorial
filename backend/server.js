edf8921e8c6f498cb87427a3db12268781d618d3
Mohamad Shalihin Bin Ahmad <shalihin@Mohamads-MacBook-Pro.local>
2022-08-26 11:10:22 +0200

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,30 @@
 require("dotenv").config();
 const express = require("express");
+const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
 
 // express app
 const app = express();
 
-// routes
-app.get("/", (req, res) => {
-  res.json({ mssg: "Welcome to the app!" });
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("Listening on port ", process.env.PORT);
-});
+//routes
+app.use("/api/workouts", workoutRoutes);
+
+//connect to db
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to DB. Listening on port: " + process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
